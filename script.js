877a2959f71444004d04da17c975fa854cdcf772
JyojiTenguam <j.y.oji@hotmail.com>
2023-12-05 21:42:38 -0300

    Resolvendo requisito 04 do DOM manipulation

--- script.js
+++ script.js
@@ -3,24 +3,14 @@
 // exercicio 01
 
 // Função para alterar o texto da tag <p>
-function alterarTexto() {
-  // Seleciona a segunda tag <p> dentro da classe "center-content"
-  const paragrafo = document.querySelector('.center-content p:nth-child(2)');
-
-  // Verifica se o elemento foi encontrado
-  if (paragrafo) {
-    // Muda o texto dentro da tag <p>
-    paragrafo.innerHTML = 'Me vejo trabalhando como full stack.';
-  } else {
-    console.error('Elemento não encontrado.');
-  }
-}
-
-// Chama a função quando a página é carregada
-window.onload = alterarTexto;
+const texto = 'Me vejo trabalhando como full stack.';
+document.querySelector('.center-content p:nth-child(2)').innerText = texto;
 
 // exercicio 02
 document.querySelector('.main-content').style.backgroundColor = 'rgb(76, 164, 109)';
 
 // exercicio 03
 document.querySelector('.center-content').style.backgroundColor = 'white';
+
+// exercicio 04
+document.querySelector('.title').innerText = 'Desafio - JavaScript';
